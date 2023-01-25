ac8ff4a22731b36930a8271c5db30bf83ee84cd7
ak-fishbarc <rybarczyk.ak@gmail.com>
2023-01-25 22:36:47 +0000

    Added temporary database tests. Changed core to accomodate future refactoring. Currently I'm thinking about changing the structure of the app, right now it's not practical for building testing around database. As database is fixed to the initial context. Testing is using app and db made in core file, because of that, db is fixed to app.db. It's not a big deal for this project, but it would be better if db could be made separately for testing. Main issue is Models file, as Model for Users table is fixed to db.

--- rogue_core.py
+++ rogue_core.py
@@ -7,16 +7,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-Bootstrap(app)
-db = SQLAlchemy(app)
-# Amazing solution to circular dependency... <3
-db.relationship('User')
-migrate = Migrate(app, db)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    Bootstrap(app)
+    app.register_blueprint(routes)
+    app.register_blueprint(forms)
 
-app.register_blueprint(routes)
-app.register_blueprint(forms)
+    return app
+
+
+def create_db(app):
+    db = SQLAlchemy(app)
+    # Amazing solution to circular dependency... <3
+    db.relationship('User')
+    migrate = Migrate(app, db)
+
+    return db
+
+
+app = create_app()
+db = create_db(app)
 
 
 if __name__ == "__main__":
--- test_core/unit_suite/test_back.py
+++ test_core/unit_suite/test_back.py
@@ -1,13 +1,19 @@
 import unittest
 
 from werkzeug.test import Client
-from rogue_core import app
+from rogue_core import app, db
+from models import User
 
 
 class TestBack(unittest.TestCase):
 
     def setUp(self):
+        app.app_context().push()
         self.server = Client(app)
+        db.create_all()
+
+    def tearDown(self):
+        db.drop_all()
 
     def test_back_of_home(self):
         response = self.server.get('/')
@@ -21,6 +27,15 @@ class TestBack(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('form', response.data.decode())
 
+    def test_db_user_model(self):
+        u = User(username="DecardCain", email="DecardCain@example.co.uk")
+        db.session.add(u)
+        db.session.commit()
+
+        query_users = User.query.all()
+        self.assertIn("DecardCain", query_users[0].username)
+
 
 if __name__ == '__main__':
     unittest.main()
+
