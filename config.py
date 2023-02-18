ad8355bb8c73404b70a4dee044bc660232e028ff
ak-fishbarc <rybarczyk.ak@gmail.com>
2023-02-18 21:46:37 +0000

    Added separate database for game editor. Added tests for database model

--- config.py
+++ config.py
@@ -6,8 +6,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_BINDS = {
+        "game_db": 'sqlite:///' + os.path.join(basedir, 'game.db')
+    }
     SECRET_KEY = "just_for_now"
 
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
+    SQLALCHEMY_BINDS = {
+        "game_db": 'sqlite:///' + os.path.join(basedir, 'test_game.db')
+    }
